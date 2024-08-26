fc7e6e89a95a5380702f57e283a03389d7cf2ea6
Jubayer Ahmed <itsjubayer@gmail.com>
2024-08-26 13:58:55 +0600

    Add project files.

---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortalAPI.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; } ///? makes it nullable 
+        public decimal Salary { get; set; }
+    }
+}
---
+++ Models/UpdateEmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortalAPI.Models
+{
+    public class UpdateEmployeeDto
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; } ///? makes it nullable 
+        public decimal Salary { get; set; }
+    }
+}
